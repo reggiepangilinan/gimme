8ba451a3af99d5d0fbbe387dc1f72393b1e5bca6
Reggie Pangilinan <reggiepangilinan@gmail.com>
2020-10-13 20:46:45 +1100

    Setup projects

---
+++ Gimme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gimme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
