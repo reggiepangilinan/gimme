06f99c766912a292b570d8a56c014a4940cb6eb6
Reggie Pangilinan <reggiepangilinan@gmail.com>
2020-10-19 13:58:16 +1100

    Handle command parsing error globally

--- Gimme/Program.cs
+++ Gimme/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Gimme.Commands;
+using Gimme.Extensions;
 using Gimme.Services;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,23 +15,25 @@ namespace Gimme
         static async Task Main(string[] args)
         {
 
-           // Register services 
-           ServiceCollection services = new ServiceCollection();
-           services.AddSingleton<IFileSystemService,FileSystemService>();
-           services.AddSingleton<JsonSerializerOptions>( _ => new JsonSerializerOptions(){
-               PropertyNameCaseInsensitive = false,
-               ReadCommentHandling = JsonCommentHandling.Skip,
-               WriteIndented = true
-           });
-           ServiceProvider servideProvider = services.BuildServiceProvider();
+            // Register services 
+            ServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IFileSystemService, FileSystemService>();
+            services.AddSingleton<JsonSerializerOptions>(_ => new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = false,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                WriteIndented = true
+            });
+            ServiceProvider servideProvider = services.BuildServiceProvider();
 
-           // Initialize the main command line application
-           var app = new CommandLineApplication<GimmeCommand>();
-           app.Conventions.UseDefaultConventions()
-                          .UseConstructorInjection(servideProvider);;
+            // Initialize the main command line application
+            var app = new CommandLineApplication<GimmeCommand>();
+            app.Conventions.UseDefaultConventions()
+                           .UseConstructorInjection(servideProvider);
 
             // Read gimmeSettings
             var fileSystemService = servideProvider.GetService<IFileSystemService>();
+            var console = servideProvider.GetService<IConsole>() ?? PhysicalConsole.Singleton;
 
             // Dynamically build sub commands
             var dynamicSubCommand = new CommandLineApplication();
@@ -37,18 +41,32 @@ namespace Gimme
             dynamicSubCommand.Name = "dynamic";
             dynamicSubCommand.Description = "⚡️ Some description about the command";
 
-            var subjectOption = new CommandOption("-s|--subject",CommandOptionType.SingleValue);
+            var subjectOption = new CommandOption("-s|--subject", CommandOptionType.SingleValue);
             subjectOption.IsRequired(allowEmptyStrings: false, $"{subjectOption.LongName} is required.");
             subjectOption.Description = "Some option";
 
             dynamicSubCommand.Options.Add(subjectOption);
-            dynamicSubCommand.OnExecuteAsync(cancellationToken => {
-                return Task.Run(()=> Console.WriteLine(@"Hello from dynamic command 👋")); 
+            dynamicSubCommand.OnExecuteAsync(cancellationToken =>
+            {
+                return Task.Run(() => Console.WriteLine(@"Hello from dynamic command 👋"));
             });
             app.AddSubcommand(dynamicSubCommand);
 
-           // Run application                          
-           await app.ExecuteAsync(args);
+            try
+            {
+                // Run application                          
+                await app.ExecuteAsync(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                console.WriteLineError(ex.Message);
+
+                if (ex is UnrecognizedCommandParsingException uex && uex.NearestMatches.Any())
+                {
+                    console.WriteLineInfo("Did you mean this?");
+                    console.WriteLineInfo("    " + uex.NearestMatches.First());
+                }
+            }
         }
     }
 
